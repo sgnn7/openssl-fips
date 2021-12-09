ce3e09971a5b4115b527ce3dc72523013a3ff56e
Srdjan Grubor <sgnn7@sgnn7.org>
2021-12-09 09:39:43 -0600

    [Windows] WIP - Get OpenSSL to build against the FIPS container
    In-progress work - still needs a lot of fixes but it's a good skeleton
    to start with.

--- canister_extract_and_build.sh
+++ canister_extract_and_build.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
 
 set -euo pipefail
 
-# Exact installation steps from security policy:
+# Exact build steps from security policy:
 # https://csrc.nist.gov/CSRC/media/projects/cryptographic-module-validation-program/documents/security-policies/140sp4024.pdf
-
+#
+# ---------------- DO NOT MODIFY LINES BELOW HERE ----------------
 tar -zxf OpenSSL_2.0.13_OracleFIPS_1.0.tar.gz
 
 cd "OracleFIPS_1.0"
@@ -12,3 +13,23 @@ cd "OracleFIPS_1.0"
 ./config
 make
 make install
+# ---------------- DO NOT MODIFY LINES ABOVE HERE ----------------
+
+
+# Copy required files to the root of the drive for consistency
+# across platforms
+
+case "$(uname -s)" in
+  MSYS*) root_dir="/c";;
+  *)     root_dir="/";;
+esac
+
+target_dir="${root_dir}/usr/local"
+if [ ! -d "${target_dir}/ssl" ]; then
+  echo "Copying build artifacts to ${target_dir}..."
+  mkdir -p "${target_dir}"
+  cp -r "/usr/local/ssl" "${target_dir}"
+fi
+
+echo "Cleaning up..."
+rm -rf "OracleFIPS_1.0"
